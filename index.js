7ee9ab488669b20178d0e22d3d3f5a0fb790fa18
Sors <sors.pet@gmail.com>
2017-05-03 22:25:19 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<html>
+<head>
+    <title>SORS</title>
+</head>
+<body>
+    <%
+        String redirectURL = "http://www.sors.ml/";
+        response.sendRedirect(redirectURL);
+    %>
+</body>
+</html>
