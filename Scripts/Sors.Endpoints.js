4a0cc99a89c8856df245c9bf3a9cb6f0e31dda09
Sors <12751190+sorset@users.noreply.github.com>
2025-11-09 18:24:26 +0200

    Add files via upload

--- Scripts/Sors.Endpoints.js
+++ Scripts/Sors.Endpoints.js
@@ -1,1 +1,192 @@
-.
+
+// service bus
+
+
+
+/* SORS */
+
+"use strict";
+
+; (function (window, document, undefined) {
+    var tests = [];
+
+    /**
+     *
+     * SorsEndpoint is the constructor for SorsEndpoint
+     *
+     * @class
+     * @access public
+     */
+
+    var SorsEndpointPrototype = {
+        _version: '1.0',
+        _config: {
+            'classPrefix': '',
+            'enableClasses': true,
+            'enableJSClass': true,
+            'usePrefixes': true
+        },
+
+        _q: [],
+        _delay: 400,
+
+        Gate: function () {
+            return '256.12.12.3';
+        },
+
+        // BG
+        Server1: function () {
+            return '156.12.12.1';
+        },
+
+        // US - FACEBOOK CENTR
+        Server2: function () {
+            return '156.102.12.1';
+        },
+
+        // RU
+        Server3: function () {
+            return '200.12.120.1';
+        },
+
+        // INDIA
+        Server4: function () {
+            return '233.92.12.1';
+        },
+
+        // INDIA2
+        Server5: function () {
+            return '10.5.185.1';
+        },
+
+        // INDIA3
+        Server6: function () {
+            return '88.77.200.1';
+        },
+
+
+
+
+
+
+        GateDomain: function () {
+            return 'sub.bg';
+        },
+
+        // BG
+        Server1Domain: function () {
+            return 'sorset.github.io';
+        },
+
+        // US - FACEBOOK CENTR
+        Server2Domain: function () {
+            return 'sorsconstruct.github.io';
+        },
+
+        // RU
+        Server3Domain: function () {
+            return 'sors.somee.com';
+        },
+
+        // INDIA
+        Server4Domain: function () {
+            return 'sub.i';
+        },
+
+        // INDIA2
+        Server5Domain: function () {
+            return 'sub.in';
+        },
+
+        // INDIA3
+        Server6Domain: function () {
+            return 'sub.india';
+        },
+
+        // Amazon 
+        Server7Domain: function () {
+            return 'sub.de';
+        },
+
+
+        Server01LocalDomain: function () {
+            return 'localhost';
+        },
+
+        Server02LocalDomain: function () {
+            return 'localhost/Sors.Subtitles';
+        },
+
+
+
+        // Free public
+        Server01Domain: function () {
+            return 'subsdiving.somee.com';
+        },
+
+        Server02Domain: function () {
+            return 'aisub.bsite.net';
+        },
+
+        Server03Domain: function () {
+            return 'subsdiv.info';
+        },
+
+        Server04Domain: function () {
+            return 'pzprovi.github.io';
+        },
+
+        Server05Domain: function () {
+            return 'subtitles.free.bg';
+        },
+
+
+
+    };
+
+
+
+    // Fake some of Object.create so we can force non test results to be non "own" properties.
+    var SorsEndpoint = function () { };
+    SorsEndpoint.prototype = SorsEndpointPrototype;
+
+    SorsEndpoint = new SorsEndpoint();
+
+    /**
+     * is returns a boolean if the typeof an obj is exactly type.
+     *
+     * @access private
+     * @function is
+     * @param {*} obj - A thing we want to check the type of
+     * @param {string} type - A string to compare the typeof against
+     * @returns {boolean}
+     */
+
+    function is(obj, type) {
+        return typeof obj === type;
+    }
+    ;
+
+    var classes = [];
+
+
+    /**
+     * Run through all tests and detect their support in the current UA.
+     * @access private
+     */
+
+
+
+
+
+    function test1() {
+
+    }
+
+
+
+    window.SorsEndpoint = SorsEndpoint;
+
+})(window, document);
+
+
---
+++ Scripts/Sors.LoadBalancer.js
@@ -0,0 +1,261 @@
+/* SORS */
+
+"use strict";
+
+; (function (window, document, undefined) {
+    var tests = [];
+
+    /**
+     *
+     * SorsLoadBalancer is the constructor for SorsLoadBalancer
+     *
+     * @class
+     * @access public
+     */
+
+    var SorsLoadBalancerPrototype = {
+        _version: '1.0',
+        _config: {
+            'classPrefix': '',
+            'enableClasses': true,
+            'enableJSClass': true,
+            'usePrefixes': true
+        },
+
+        _q: [],
+        _delay: 400,
+        _mirrorCheck: 0,
+        _lock: false,
+
+
+        loadBalancer: function (queryParametars) {
+
+            var urlPage1 = "https://" + SorsEndpoint.Server1Domain() + "/";
+            var urlPage2 = "https://" + SorsEndpoint.Server2Domain() + "/";
+            var urlPage3 = "https://" + SorsEndpoint.Server3Domain() + "/";
+
+            var isUrl1Active = null;
+            var isUrl2Active = null;
+            var isUrl3Active = null;
+
+            var request = new XMLHttpRequest();
+            var request2 = new XMLHttpRequest();
+            var request3 = new XMLHttpRequest();
+
+            var currentUrlPage = location.protocol + "//" + $(location).attr('hostname') + "/";
+
+            request.open('GET', urlPage1, true);
+            request.onerror = function () {
+                isUrl1Active = false;
+                Balancer._mirrorCheck++;
+            };
+            request.onload = function () {
+                if (this.status === 404) {
+                    isUrl1Active = false;
+                    Balancer._mirrorCheck++;
+                    return;
+                }
+                isUrl1Active = true;
+            };
+            request.onreadystatechange = function () {
+                if (request.readyState === 4 && request.status === 200) {
+                    Balancer._mirrorCheck++;
+                    isUrl1Active = false;
+                }
+                else if (request.readyState === 4 && request.status === 0) {
+                    isUrl1Active = false;
+                    return;
+                }
+            };
+
+            request2.open('GET', urlPage2, true);
+            request2.onerror = function () {
+                isUrl2Active = false;
+                Balancer._mirrorCheck++;
+            };
+            request2.onload = function () {
+                if (this.status === 404) {
+                    isUrl2Active = false;
+                    Balancer._mirrorCheck++;
+                    return;
+                }
+                isUrl2Active = true;
+            };
+            request2.onreadystatechange = function () {
+                if (request2.readyState === 4 && request2.status === 200) {
+                    Balancer._mirrorCheck++;
+                    isUrl2Active = false;
+                }
+                else if (request2.readyState === 4 && request2.status === 0) {
+                    isUrl2Active = false;
+                    return;
+                }
+            };
+
+            request3.open('GET', urlPage3, true);
+            request3.onerror = function () {
+                isUrl3Active = false;
+                Balancer._mirrorCheck++;
+            };
+            request3.onload = function () {
+                if (this.status === 404) {
+                    isUrl3Active = false;
+                    Balancer._mirrorCheck++;
+                    return;
+                }
+                isUrl3Active = true;
+            };
+            request3.onreadystatechange = function () {
+                if (request3.readyState === 4 && request3.status === 200) {
+                    Balancer._mirrorCheck++;
+                    isUrl3Active = false;
+                }
+                else if (request3.readyState === 4 && request3.status === 0) {
+                    isUrl3Active = false;
+                    return;
+                }
+            };
+
+            // request.send();
+            request2.send();
+            request3.send();
+
+
+
+            const myRefreshFunction = function () {
+                setTimeout(function () {
+
+                    if (_lock == false && alancer._mirrorCheck >= 2) {
+                        _lock = true;
+
+                        if (isUrl1Active) {
+                            window.location.href = urlPage1 + queryParametars;
+                        }
+                        else if (isUrl2Active) {
+                            window.location.href = urlPage2 + queryParametars;
+                        }
+                        else if (isUrl3Active) {
+                            window.location.href = urlPage3 + queryParametars;
+                        }
+                    }
+                    else {
+                        myRefreshFunction();
+                    }
+
+                }, 500);
+            };
+
+            myRefreshFunction();
+
+
+        },
+
+
+        redirect: function (urlPage1, queryParametars) {
+
+            let currentUrlPage = location.protocol + "//" + $(location).attr('hostname') + "/";
+
+            if (currentUrlPage == urlPage1 && queryParametars)
+                window.location.href = urlPage1 + queryParametars + "#about";
+            else if (currentUrlPage == urlPage1)
+                window.location.href = urlPage1;
+        },
+
+        test2: function () {
+
+
+        },
+
+
+
+
+
+
+    };
+
+
+
+    // Fake some of Object.create so we can force non test results to be non "own" properties.
+    var SorsLoadBalancer = function () { };
+    SorsLoadBalancer.prototype = SorsLoadBalancerPrototype;
+
+    // Leak SorsLoadBalancer globally when you `require` it rather than force it here.
+    // Overwrite name so constructor name is nicer :D
+    SorsLoadBalancer = new SorsLoadBalancer();
+
+
+
+    /**
+     * is returns a boolean if the typeof an obj is exactly type.
+     *
+     * @access private
+     * @function is
+     * @param {*} obj - A thing we want to check the type of
+     * @param {string} type - A string to compare the typeof against
+     * @returns {boolean}
+     */
+
+    function is(obj, type) {
+        return typeof obj === type;
+    }
+    ;
+
+    var classes = [];
+
+
+    /**
+     * Run through all tests and detect their support in the current UA.
+     *
+     * @access private
+     */
+
+    function testRunner() {
+        var featureNames;
+        var feature;
+
+
+
+
+    }
+
+    // Run each test
+    /*
+    testRunner();
+
+    delete SorsLoadBalancerPrototype.addTest;
+    delete SorsLoadBalancerPrototype.addAsyncTest;
+
+    // Run the things that are supposed to run after the tests
+    for (var i = 0; i < SorsLoadBalancer._q.length; i++)
+    {
+        SorsLoadBalancer._q[i]();
+    }
+    */
+
+    // Leak SorsLoadBalancer namespace
+    window.SorsLoadBalancer = SorsLoadBalancer;
+
+})(window, document);
+
+//(function ($) {
+
+//    $(window).load(function () {
+
+//    });
+
+
+//    $(document).load(function () {
+
+//    });
+
+//    $(document).ready(function () {
+
+//    });
+
+
+//    $(window).bind("load", function () {
+//        // SorsLoadBalancer.test();
+//    });
+
+//})($);
+
