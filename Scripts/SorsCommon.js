1c0205299b548caba7ccebbbe53016b447aa3d2d
Sors <12751190+sorset@users.noreply.github.com>
2026-07-08 16:27:52 +0300

    Add files via upload

--- Scripts/SorsCommon.js
+++ Scripts/SorsCommon.js
@@ -23,7 +23,7 @@ function getParameterByName(name, url) {
     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
         results = regex.exec(url);
     if (!results)
-        return null;
+        return '';
     if (!results[2])
         return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
